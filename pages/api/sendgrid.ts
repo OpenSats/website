0450967717561be4b98106bab58f2aa553ad505a
Gigi <dergigi@pm.me>
2023-05-01 23:35:06 +0200

    Fix typo in env var
    Copied env var in Development, Preview, and Production. We now have one with the typo and one without. Will remove the typo one upon successful test & deploy.
    
    Fixes #57

--- pages/api/sendgrid.ts
+++ pages/api/sendgrid.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next/types'
 
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
-const TO_ADDRESS = process.env.SENDGRID_RECEPIENT
+const TO_ADDRESS = process.env.SENDGRID_RECIPIENT
 const CC_ADDRESS = process.env.SENDGRID_CC
 const FROM_ADDRESS = process.env.SENDGRID_VERIFIED_SENDER
 
