fc002081397f1eb69cc71edf62367ae419e1b4a7
Gigi <dergigi@pm.me>
2023-05-16 01:22:44 +0200

    Nav link to /apply

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: 'https://opensats.org/projects', title: 'Projects' },
-  { href: 'https://opensats.org/apply', title: 'Apply' },
+  { href: '/apply', title: 'Apply' },
   { href: 'https://opensats.org/faq', title: 'FAQ' },
   { href: '/about', title: 'About Us' },
 ]
