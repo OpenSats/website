b61e8af95655bf4c603edd0380fcaf2f3f540954
Gigi <dergigi@pm.me>
2023-05-16 00:01:50 +0200

    Update nav links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,9 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: 'https://opensats.org/projects', title: 'Projects' },
+  { href: 'https://opensats.org/apply', title: 'Apply' },
+  { href: 'https://opensats.org/faq', title: 'FAQ' },
+  { href: 'https://opensats.org/about', title: 'About Us' },
 ]
 
 export default headerNavLinks
