d39228608ef43d53213f46c9f5bf74a16c1c838f
Gigi <dergigi@pm.me>
2023-05-16 23:58:08 +0200

    Add FAQ

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: 'https://opensats.org/projects', title: 'Projects' },
   { href: '/apply', title: 'Apply' },
-  { href: 'https://opensats.org/faq', title: 'FAQ' },
+  { href: '/faq', title: 'FAQ' },
   { href: '/about', title: 'About' },
 ]
 
