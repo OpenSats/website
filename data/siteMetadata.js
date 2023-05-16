e40c54cf2eae4ffbf7717cab0c301e2f9b75b4ef
Gigi <dergigi@pm.me>
2023-05-16 20:37:22 +0200

    Update site URL and repo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,8 +8,8 @@ const siteMetadata = {
   description: 'Updates from a better tomorrow, today.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://opensats.org',
+  siteRepo: 'https://github.com/OpenSats/website/',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
