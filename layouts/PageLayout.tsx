f5213b10bb9ab64c41340fa2f79701413d52d944
Gigi <dergigi@pm.me>
2023-05-16 23:32:20 +0200

    Add PageLayout

---
+++ layouts/PageLayout.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+import type { Pages } from 'contentlayer/generated'
+import SocialIcon from '@/components/social-icons'
+import Image from '@/components/Image'
+import { PageSEO } from '@/components/SEO'
+import { CoreContent } from 'pliny/utils/contentlayer'
+
+interface Props {
+  children: ReactNode
+  // content: Omit<Pages, '_id' | '_raw' | 'body'>
+  content: CoreContent<Pages>
+}
+
+export default function PageLayout({ children, content }: Props) {
+  const { title, date, summary, layout } = content
+
+  return (
+    <>
+      <PageSEO title={`${title} - OpenSats`} description={`${summary}`} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            {title}
+          </h1>
+        </div>
+        <div className="prose text-gray-500 dark:text-gray-400">
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
