7188e916cba95810f145ae2c6ee7c1797bb6613b
Gigi <dergigi@pm.me>
2023-05-16 23:34:37 +0200

    Add /mission to nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,4 +1,5 @@
 const headerNavLinks = [
+  { href: '/mission', title: 'Mission' },
   { href: '/blog', title: 'Blog' },
   { href: 'https://opensats.org/projects', title: 'Projects' },
   { href: '/apply', title: 'Apply' },
