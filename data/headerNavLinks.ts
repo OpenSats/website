60f56ca98fe0bcc658fb43b06b261911aa5c8297
Gigi <dergigi@pm.me>
2023-05-17 00:06:51 +0200

    Re-order header links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,9 +2,9 @@ const headerNavLinks = [
   { href: '/mission', title: 'Mission' },
   { href: '/blog', title: 'Blog' },
   { href: 'https://opensats.org/projects', title: 'Projects' },
-  { href: '/apply', title: 'Apply' },
   { href: '/faq', title: 'FAQ' },
   { href: '/about', title: 'About' },
+  { href: '/apply', title: 'Apply' },
 ]
 
 export default headerNavLinks
