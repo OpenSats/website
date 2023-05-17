d15e282f9daf0949f301c419ed2531182a5d4ef1
Gigi <dergigi@pm.me>
2023-05-17 21:36:06 +0200

    Use grid for donate button and detail link

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -50,7 +50,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         <div className="flex justify-end"></div>
 
         <ShareButtons project={project} />
-        <div className="flex space-x-4 items-center justify-left pt-4">
+        <div className="grid grid-cols-2">
           <button
             className="bg-transparent hover:bg-stone-800 text-stone-800 font-semibold hover:text-stone-100 py-2 px-4 border border-stone-800 hover:border-transparent rounded"
             onClick={() => openPaymentModal(project)}
