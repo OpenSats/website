4b49a0723c5a82cb90edb9502ce896c2ab64659e
Gigi <dergigi@pm.me>
2023-05-19 23:45:57 +0200

    [WIP] Cosmetics

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -32,15 +32,15 @@ export default function FourZeroFour() {
           </Link>
         </div>
       </div>
-      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-left md:justify-center md:space-x-6 border">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5 text-right border">
+      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-left md:justify-center md:space-x-6">
+        <div className="space-x-2 pt-6 pb-8 md:space-y-5 text-right">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
             OSG
           </h1>
         </div>
-        <div className="max-w-md border">
+        <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            OpenSats Project Listing
+            OpenSats Grant
           </p>
           <p className="mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -82,7 +82,7 @@ export default function FourZeroFour() {
             href="/"
             className="focus:shadow-outline-orange inline rounded-lg border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-orange-700 focus:outline-none dark:hover:bg-orange-500"
           >
-            Apply for a Long-Term Grant
+            Apply for an OpenSats LTS Grant
           </Link>
         </div>
       </div>
