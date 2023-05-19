c73de094c0e41656ed96b76bf06ca0d94fa50db4
Gigi <dergigi@pm.me>
2023-05-19 17:54:49 +0200

    Add list of all board members to "About Us"

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,16 +7,27 @@ import { MDXComponents } from '@/components/MDXComponents'
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
 export const getStaticProps = async () => {
-  const author = allAuthors.find((p) => p.slug === 'default')
-  return { props: { author } }
+  const openSats = allAuthors.find((p) => p.slug === 'default')
+  const board = allAuthors.filter((p) => p.board === true)
+  return { props: { openSats, board } }
 }
 
-export default function About({ author }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function About({ openSats, board }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
+    <>
     <MDXLayoutRenderer
-      layout={author.layout || DEFAULT_LAYOUT}
-      content={author}
+      layout={openSats.layout || DEFAULT_LAYOUT}
+      content={openSats}
       MDXComponents={MDXComponents}
     />
+    {/* List all members of the board */}
+    {board.map((member, i) => 
+      (
+        <MDXLayoutRenderer
+          layout={member.layout || DEFAULT_LAYOUT}
+          content={member}
+          MDXComponents={MDXComponents} />
+      ))}
+    </>
   )
 }
