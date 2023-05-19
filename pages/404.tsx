45b8280a332f33574a9f451e6b9b8cc7a2a29d21
Gigi <dergigi@pm.me>
2023-05-19 23:18:45 +0200

    Fix button color

--- pages/404.tsx
+++ pages/404.tsx
@@ -20,7 +20,7 @@ export default function FourZeroFour() {
           </p>
           <Link
             href="/"
-            className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+            className="focus:shadow-outline-orange inline rounded-lg border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-orange-700 focus:outline-none dark:hover:bg-orange-500"
           >
             Back to homepage
           </Link>
