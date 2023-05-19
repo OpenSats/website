42d5b7abdd6dea56885e7bc3b42463d843e1d3d9
Gigi <dergigi@pm.me>
2023-05-19 21:02:33 +0200

    Improve Board of Directors layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -23,22 +23,32 @@ export default function About({ openSats, board }: InferGetStaticPropsType<typeo
       MDXComponents={MDXComponents}
     />
     {/* List all members of the board */}
-    <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 pt-6 pb-8 md:space-y-5">
+        <div></div>
+        <h1 className="text-3xl xl:col-span-2 font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Board of Directors
+        </h1>
+      </div>
+      <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+      <div className="col-start-2 col-span-2 space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 ">
     {board.map((member, i) => 
       (
-        <div className="flex flex-col items-center space-x-2 pt-8">
+        <div className="flex flex-col items-left space-x-2 pt-8">
           <Link href={`/team/${member.slug}`}>
             <Image
               src={member.avatar}
-              alt="avatar"
-              width={192}
-              height={192}
-              className="h-48 w-48 rounded-full"
+              alt={member.name}
+              width={120}
+              height={120}
+              className="h-36 w-36 rounded-full"
             />
           </Link>
         </div>
       ))}
+        </div>
       </div>
+    </div>
     </>
   )
 }
