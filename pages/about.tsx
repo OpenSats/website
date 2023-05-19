6d06a9f9a47ec9873e253dd0dfd5dac5a83f3c81
Gigi <dergigi@pm.me>
2023-05-19 20:52:00 +0200

    Display board as 3x3 grid

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,8 @@ import { InferGetStaticPropsType } from 'next'
 import { allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import { MDXComponents } from '@/components/MDXComponents'
+import Image from '@/components/Image'
+import Link from '@/components/Link'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
@@ -21,13 +23,22 @@ export default function About({ openSats, board }: InferGetStaticPropsType<typeo
       MDXComponents={MDXComponents}
     />
     {/* List all members of the board */}
+    <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
     {board.map((member, i) => 
       (
-        <MDXLayoutRenderer
-          layout={member.layout || DEFAULT_LAYOUT}
-          content={member}
-          MDXComponents={MDXComponents} />
+        <div className="flex flex-col items-center space-x-2 pt-8">
+          <Link href={`/team/${member.slug}`}>
+            <Image
+              src={member.avatar}
+              alt="avatar"
+              width={192}
+              height={192}
+              className="h-48 w-48 rounded-full"
+            />
+          </Link>
+        </div>
       ))}
+      </div>
     </>
   )
 }
