1d08feabc1c978de182bdd842737cca311395dc6
Gigi <dergigi@pm.me>
2023-05-22 20:17:46 +0200

    'LTS Grant' text

--- pages/apply/index.tsx
+++ pages/apply/index.tsx
@@ -75,13 +75,10 @@ export default function Apply({ apply }: InferGetStaticPropsType<typeof getStati
               Long-Term Support
             </h2>
             <p className="mb-8">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              We have a limited number of long-term support grants available for
+              projects that are critical to the Bitcoin ecosystem. These grants
+              are geared towards developers and maintainers of Bitcoin Core and
+              similar.
             </p>
             <Link
               href="/apply/lts"
