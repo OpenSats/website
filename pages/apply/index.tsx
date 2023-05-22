bdcd2634605de502b6c9d52d12d682e46318b3e6
Gigi <dergigi@pm.me>
2023-05-22 17:58:08 +0200

    'General Grant' text

--- pages/apply/index.tsx
+++ pages/apply/index.tsx
@@ -60,13 +60,10 @@ export default function Apply({ apply }: InferGetStaticPropsType<typeof getStati
               General Grant
             </h2>
             <p className="mb-8">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              General grants are funded by donations to the OpenSats General
+              Fund and will be distributed quarterly by our board. We evaluate
+              and assess all applications to make sure grants are awarded to
+              high impact projects in the Bitcoin space. 
             </p>
             <Link
               href="/apply/legacy"
