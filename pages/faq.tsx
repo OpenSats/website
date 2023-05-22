16adc1fba204ecfde41f7730b4181055aa71d3e8
Gigi <dergigi@pm.me>
2023-05-22 16:17:19 +0200

    Fix function name

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -11,7 +11,7 @@ export const getStaticProps = async () => {
   return { props: { page: page } }
 }
 
-export default function Mission({ page }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function FAQ({ page }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <MDXLayoutRenderer
       layout={page.layout || DEFAULT_LAYOUT}
