e1c87f992df47cd104959bb9a3d786af824adb9f
Gigi <dergigi@pm.me>
2023-05-23 22:24:03 +0200

    Remove unused ContentLayer (Projects)

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,8 @@
-import { defineDocumentType, ComputedFields, makeSource } from 'contentlayer/source-files'
+import {
+  defineDocumentType,
+  ComputedFields,
+  makeSource,
+} from 'contentlayer/source-files'
 import readingTime from 'reading-time'
 import path from 'path'
 // Remark packages
@@ -91,30 +95,9 @@ export const Pages = defineDocumentType(() => ({
   computedFields,
 }))
 
-export const Projects = defineDocumentType(() => ({
-  name: 'Project',
-  filePathPattern: 'projects/**/*.mdx',
-  contentType: 'mdx',
-  fields: {
-    title: { type: 'string', required: true },
-    summary: { type: 'string', required: true },
-    nym: { type: 'string', required: true },
-    coverImage: { type: 'string', required: true },
-    website: { type: 'string', required: true },
-    git: { type: 'string', required: true },
-    twitter: { type: 'string', required: true },
-    personalTwitter: { type: 'string', required: true },
-    zaprite: { type: 'string', required: true },
-    btcpay: { type: 'string', required: true },
-    bonusUSD: { type: 'number' },
-    hidden: { type: 'boolean' },
-  },
-  computedFields,
-}))
-
 export default makeSource({
   contentDirPath: 'data',
-  documentTypes: [Blog, Authors, Pages, Projects],
+  documentTypes: [Blog, Authors, Pages],
   mdx: {
     cwd: process.cwd(),
     remarkPlugins: [
