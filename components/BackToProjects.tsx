58bf093d69d2bcc5c08ed0f427677489e7d8abae
Gigi <dergigi@pm.me>
2023-05-23 22:02:53 +0200

    Use HTML arrow instead of FontAwesome

--- components/BackToProjects.tsx
+++ components/BackToProjects.tsx
@@ -1,19 +1,14 @@
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 const BackToProjects = () => {
   return (
     <div className="flex items-center pb-4">
-      <FontAwesomeIcon
-        icon={faArrowLeft}
-        className="text-primary mr-1 h-4 w-4"
-      />
       <Link
         href="/projects"
         aria-label="Back to projects"
         className="text-primary"
       >
+        &larr;{' '}
         Back to projects
       </Link>
     </div>
