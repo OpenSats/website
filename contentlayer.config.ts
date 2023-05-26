8ef14c3d997cd4a7bbbecddc0f8c9e813f6b3108
Gigi <dergigi@pm.me>
2023-05-26 15:49:46 +0200

    Add ops team

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -78,6 +78,7 @@ export const Authors = defineDocumentType(() => ({
     github: { type: 'string' },
     layout: { type: 'string' },
     board: { type: 'boolean' },
+    ops: { type: 'boolean' },
   },
   computedFields,
 }))
