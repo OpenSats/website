7ec76c0e16ab563c359fd3773dbb4df5c0e1c6ac
Gigi <dergigi@pm.me>
2023-06-11 17:01:21 +0200

    Add "Freedom Tools" to typing list

--- components/Typing.tsx
+++ components/Typing.tsx
@@ -10,6 +10,7 @@ export default function Typing() {
         'Bitcoin',
         'Contributors',
         'FOSS',
+        'Freedom Tools',
         'Freedom',
         'Developers',
         'Decentralization',
