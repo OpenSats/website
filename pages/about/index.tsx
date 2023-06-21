7535bd83e5660b119a08452f65f4ca1e5b8383fb
Gigi <dergigi@pm.me>
2023-06-21 20:34:11 +0200

    Remove ops

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -59,31 +59,6 @@ export default function About({
         <div className="items-start space-y-2 pb-8 pt-6 md:space-y-5 xl:grid xl:grid-cols-3 xl:gap-x-8">
           <div></div>
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 xl:col-span-2">
-            Operations
-          </h1>
-        </div>
-        <div className="grid items-start space-y-2 xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="col-span-2 col-start-2 grid grid-cols-2 space-y-2 sm:gap-x-2 md:grid-cols-3 md:gap-x-8">
-            {ops.map((member, i) => (
-              <div className="items-left flex flex-col space-x-2 pt-8" key={i}>
-                <Link href={`/about/${member.slug}`}>
-                  <Image
-                    src={member.avatar}
-                    alt={member.name}
-                    width={120}
-                    height={120}
-                    className="h-36 w-36 rounded-full"
-                  />
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="items-start space-y-2 pb-8 pt-6 md:space-y-5 xl:grid xl:grid-cols-3 xl:gap-x-8">
-          <div></div>
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 xl:col-span-2">
             Supporters
           </h1>
         </div>
