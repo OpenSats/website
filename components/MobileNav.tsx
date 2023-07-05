4b69b73f2fd9439573e8018653e8ac43f9ca7139
Gigi <dergigi@pm.me>
2023-07-05 21:38:10 +0200

    Fix close button visibility

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -52,7 +52,7 @@ const MobileNav = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="text-gray-900 dark:text-gray-100"
+              className="h-8 w-8 text-gray-900 dark:text-gray-100"
             >
               <path
                 fillRule="evenodd"
