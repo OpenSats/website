6ab515e047bca2dea38a3a2077aab3541031deb5
Gigi <dergigi@pm.me>
2023-07-06 14:18:19 +0200

    Better styling for projects partial

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ import { ProjectItem } from '../utils/types'
 import PaymentModal from '../components/PaymentModal'
 import { isNotOpenSatsProject } from './projects'
 import Typing from '@/components/Typing'
+import CustomLink from '@/components/Link'
 
 const MAX_DISPLAY = 5
 
@@ -114,10 +115,10 @@ export default function Home({
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-24">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest Posts
+            Stay Updated
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Updates from the OpenSats team.
+            Read the latest posts from the OpenSats team.
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -195,7 +196,19 @@ export default function Home({
         onRequestClose={closeModal}
         project={selectedProject}
       />
-      <div className="mb-4 mt-8 divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-24">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Explore Projects
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Browse through and directly support{' '}
+            <CustomLink href="/projects" className="underline">
+              projects selected by OpenSats
+            </CustomLink>
+            .
+          </p>
+        </div>
         <ProjectList projects={projects} openPaymentModal={openPaymentModal} />
       </div>
     </>
