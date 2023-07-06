6bc61bb572fa13993293ef3a6b7dffa7c71b1fda
Gigi <dergigi@pm.me>
2023-07-06 16:35:05 +0200

    Move "Explore Projects" up again

--- pages/index.tsx
+++ pages/index.tsx
@@ -109,6 +109,21 @@ export default function Home({
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-24">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Explore Projects
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Browse through and{' '}
+            <CustomLink href="/projects" className="underline">
+              directly support projects
+            </CustomLink>{' '}
+            selected by OpenSats.
+          </p>
+        </div>
+        <ProjectList projects={projects} openPaymentModal={openPaymentModal} />
+      </div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-24">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Stay Updated
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
@@ -195,22 +210,7 @@ export default function Home({
         project={selectedProject}
       />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-24">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Explore Projects
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Browse through and directly support{' '}
-            <CustomLink href="/projects" className="underline">
-              projects selected by OpenSats
-            </CustomLink>
-            .
-          </p>
-        </div>
-        <ProjectList projects={projects} openPaymentModal={openPaymentModal} />
-      </div>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-32">
+        <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-16">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Apply for Funding
           </h1>
