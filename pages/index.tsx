a0cdf17502f6be66f622a8bacfe2a77f05cbf173
Gigi <dergigi@pm.me>
2023-07-06 14:24:44 +0200

    Add "Apply for Funding" section

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,12 +104,6 @@ export default function Home({
               </button>
             </div>
           </div>
-          <p>
-            Are you an open-source contributor?{' '}
-            <Link href="/apply" className="underline">
-              Apply for funding.
-            </Link>
-          </p>
         </div>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -210,6 +204,23 @@ export default function Home({
           </p>
         </div>
         <ProjectList projects={projects} openPaymentModal={openPaymentModal} />
+      </div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-16 md:space-y-5 xl:pt-32">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Apply for Funding
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Are you an open-source contributor? Do you align with{' '}
+            <CustomLink href="/mission" className="underline">
+              our mission
+            </CustomLink>
+            ? Are you working on Bitcoin, nostr, or freedom tech in general?{' '}
+            <CustomLink href="/apply" className="underline">
+              Apply for funding!
+            </CustomLink>
+          </p>
+        </div>
       </div>
     </>
   )
