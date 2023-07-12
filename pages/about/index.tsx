9b7de48cf6d2a2e57bd4da39b3ef6b9bd5840345
Gigi <109058+dergigi@users.noreply.github.com>
2023-07-12 18:00:30 +0200

    Add volunteers (#112)
    * Add volunteers to /about
    
    * Add volunteer images
    
    * Show names as title on hover

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,6 +5,7 @@ import { MDXComponents } from '@/components/MDXComponents'
 import Image from '@/components/Image'
 import Link from '@/components/Link'
 import Credits from '@/components/Supporters'
+import Volunteers from '@/components/Volunteers'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
@@ -53,6 +54,17 @@ export default function About({
               </div>
             ))}
           </div>
+        </div>
+      </div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="items-start space-y-2 pb-8 pt-6 md:space-y-5 xl:grid xl:grid-cols-3 xl:gap-x-8">
+          <div></div>
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 xl:col-span-2">
+            Volunteers
+          </h1>
+        </div>
+        <div className="grid items-start space-y-2 xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <Volunteers />
         </div>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
