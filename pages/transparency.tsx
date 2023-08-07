5a02ee788ff2ecb9e8fba806a99d753cdd18775a
Gigi <dergigi@pm.me>
2023-08-07 15:20:51 +0200

    Add /transparency

---
+++ pages/transparency.tsx
@@ -0,0 +1,23 @@
+import { InferGetStaticPropsType } from 'next'
+import { allPages } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import { MDXComponents } from '@/components/MDXComponents'
+
+const DEFAULT_LAYOUT = 'PageLayout'
+
+export const getStaticProps = async () => {
+  const page = allPages.find((p) => p.slug === 'transparency')
+  return { props: { page: page } }
+}
+
+export default function Transparency({
+  page,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <MDXLayoutRenderer
+      layout={page.layout || DEFAULT_LAYOUT}
+      content={page}
+      MDXComponents={MDXComponents}
+    />
+  )
+}
