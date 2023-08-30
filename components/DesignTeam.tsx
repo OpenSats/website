51b944c11317134d1772aaf21cd5db5a66606e8b
Gigi <dergigi@pm.me>
2023-08-30 14:37:21 +0200

    Remove unnecessary header

--- components/DesignTeam.tsx
+++ components/DesignTeam.tsx
@@ -18,12 +18,6 @@ export default function DesignTeam({
     <>
       {/* List all members of the design team */}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="items-start space-y-2 pb-8 pt-6 md:space-y-5 xl:grid xl:grid-cols-3 xl:gap-x-8">
-          <div></div>
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 xl:col-span-2">
-            Design Team
-          </h1>
-        </div>
         <div className="grid items-start space-y-2 xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="col-span-2 col-start-2 grid grid-cols-2 space-y-2 sm:gap-x-2 md:grid-cols-3 md:gap-x-8">
             {members.map((member, i) => (
