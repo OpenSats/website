6b00ff68edc428df74eb0a687d7c708596265cc2
Gigi <109058+dergigi@users.noreply.github.com>
2023-10-17 11:46:13 +0100

    Revert "Add project links to project view" (#158)

--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -1,25 +1,14 @@
 import { ReactNode } from 'react'
 import Image from '@/components/Image'
 import PageHeading from '@/components/PageHeading'
-import SocialIcon from '@/components/social-icons'
 
 interface Props {
   children: ReactNode
   title: string
   image: string
-  website?: string
-  git?: string
-  twitter?: string
 }
 
-export default function PageSection({
-  title,
-  image,
-  website,
-  git,
-  twitter,
-  children,
-}: Props) {
+export default function PageSection({ title, image, children }: Props) {
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <PageHeading title={title}>
@@ -31,14 +20,6 @@ export default function PageSection({
             height={210}
             className="h-48 w-48"
           />
-          <div className="flex space-x-3 pt-6">
-            <SocialIcon kind="website" href={website} />
-            <SocialIcon
-              kind="twitter"
-              href={`https://twitter.com/${twitter}`}
-            />
-            <SocialIcon kind="github" href={git} />
-          </div>
         </div>
         <div className="prose max-w-none pb-8 pt-8 dark:prose-dark xl:col-span-2">
           {children}
