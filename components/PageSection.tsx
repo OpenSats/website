7a3c25a505d1c37b0659aa145d135c1f3f723627
Jason Hong <jason.hong@protonmail.com>
2023-10-18 10:56:23 -0400

    flex unhide

--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -27,7 +27,7 @@ export default function PageSection({
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <PageHeading title={title}>
-        <div className="flex hidden flex-col items-center space-x-2 pt-8 xl:block">
+        <div className="flex flex-col items-center space-x-2 pt-8 xl:block">
           <Image
             src={image}
             alt="avatar"
