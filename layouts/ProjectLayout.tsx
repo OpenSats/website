f4050f5dffd1577ec073431867e46ece704b5bb5
Jason <126610837+moteanup24@users.noreply.github.com>
2023-10-19 17:52:11 -0400

    Add project links to project view (#163)

--- layouts/ProjectLayout.tsx
+++ layouts/ProjectLayout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react'
 import type { Project } from 'contentlayer/generated'
 import { PageSEO } from '@/components/SEO'
-import PageSection from '@/components/PageSection'
+import SocialIcon from '@/components/social-icons'
 import { CoreContent } from 'pliny/utils/contentlayer'
+import PageHeading from '@/components/PageHeading'
+import Image from '@/components/Image'
 
 interface Props {
   children: ReactNode
@@ -10,14 +12,34 @@ interface Props {
 }
 
 export default function PageLayout({ children, content }: Props) {
-  const { title, summary, coverImage } = content
-
+  const { title, summary, coverImage, website, twitter, git } = content
   return (
     <>
       <PageSEO title={`${title} - OpenSats`} description={`${summary}`} />
-      <PageSection title={title} image={coverImage}>
-        {children}
-      </PageSection>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <PageHeading title={title}>
+          <div className="flex flex-col items-center space-x-2 pt-8 xl:block">
+            <Image
+              src={coverImage}
+              alt="avatar"
+              width={210}
+              height={210}
+              className="h-48 w-48"
+            />
+            <div className="flex space-x-3 pt-6">
+              <SocialIcon kind="website" href={website} />
+              <SocialIcon
+                kind="twitter"
+                href={`https://twitter.com/${twitter}`}
+              />
+              <SocialIcon kind="github" href={git} />
+            </div>
+          </div>
+          <div className="prose max-w-none pb-8 pt-8 dark:prose-dark xl:col-span-2">
+            {children}
+          </div>
+        </PageHeading>
+      </div>
     </>
   )
 }
