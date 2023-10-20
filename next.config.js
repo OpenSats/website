18cd94eea083aa0bd4b863f6e7032cf3a09bd50c
Gigi <dergigi@pm.me>
2023-10-20 11:22:08 +0100

    Update CSP for youtube

--- next.config.js
+++ next.config.js
@@ -7,10 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app youtube.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app youtube.com https://www.youtube.com;
+  script-src-elem 'self' 'unsafe-eval' 'unsafe-inline' youtube.com http://www.youtube.com https://www.youtube.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src 'self';
+  media-src 'self' youtube.com https://www.youtube.com;
   connect-src *;
   font-src 'self';
   frame-src youtube.com https://www.youtube.com;
