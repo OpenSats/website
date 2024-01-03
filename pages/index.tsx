3c446a6f272955013fd8871e4925e90292d4912e
Gigi <dergigi@pm.me>
2024-01-03 12:18:29 +0000

    chore: wording

--- pages/index.tsx
+++ pages/index.tsx
@@ -266,10 +266,10 @@ export default function Home({
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="xl:pt-18 space-y-2 pt-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Project Showcase
+            Explore Projects
           </h1>
           <p className="pt-2 text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Browse through a showcase of select projects supported by us.
+            Browse through a showcase of projects supported by us.
           </p>
           <ProjectList
             projects={projects}
