925f59b71f3e1484ac029ef4cb245731dbf9a1bb
Gigi <dergigi@pm.me>
2024-03-15 13:10:00 +0000

    Fix /projects/all

--- pages/projects/all.tsx
+++ pages/projects/all.tsx
@@ -1,17 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
-import PaymentModal from '../../components/PaymentModal'
 import ProjectCard from '../../components/ProjectCard'
 import { allProjects } from 'contentlayer/generated'
 import { Project } from 'contentlayer/generated'
 import { isNotOpenSatsProject } from '.'
 
 const AllProjects: NextPage<{ projects: Project[] }> = ({ projects }) => {
-  const [modalOpen, setModalOpen] = useState(false)
-
-  const [selectedProject, setSelectedProject] = useState<Project>()
-
   const [sortedProjects, setSortedProjects] = useState<Project[]>()
 
   useEffect(() => {
@@ -20,19 +15,10 @@ const AllProjects: NextPage<{ projects: Project[] }> = ({ projects }) => {
     )
   }, [projects])
 
-  function closeModal() {
-    setModalOpen(false)
-  }
-
-  function openPaymentModal(project: Project) {
-    setSelectedProject(project)
-    setModalOpen(true)
-  }
-
   return (
     <>
       <Head>
-        <title>OpenSats | Projects</title>
+        <title>OpenSats | All Projects</title>
       </Head>
       <section className="flex flex-col p-4 md:p-8">
         <div className="flex w-full items-center justify-between pb-8">
@@ -42,16 +28,18 @@ const AllProjects: NextPage<{ projects: Project[] }> = ({ projects }) => {
           {sortedProjects &&
             sortedProjects.map((p, i) => (
               <li key={i} className="">
-                <ProjectCard project={p} openPaymentModal={openPaymentModal} />
+                <ProjectCard
+                  slug={`/projects/${p.slug}`}
+                  title={p.title}
+                  summary={p.summary}
+                  coverImage={p.coverImage}
+                  nym={p.nym}
+                  tags={p.tags}
+                />
               </li>
             ))}
         </ul>
       </section>
-      <PaymentModal
-        isOpen={modalOpen}
-        onRequestClose={closeModal}
-        project={selectedProject}
-      />
     </>
   )
 }
