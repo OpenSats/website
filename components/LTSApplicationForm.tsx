4b96702cf0ed75609b583368dd0513fda3cff573
Gigi <dergigi@pm.me>
2024-04-22 11:00:30 -0600

    Add "Bitcoin Core" as main focus option

--- components/LTSApplicationForm.tsx
+++ components/LTSApplicationForm.tsx
@@ -193,7 +193,8 @@ export default function ApplicationForm() {
           {...register('main_focus')}
         >
           <option value="bitcoin">Bitcoin</option>
-          <option value="lightning">Lightning</option>
+          <option value="core">Bitcoin Core</option>
+          <option value="lightning">Lightning / Layer2</option>
           <option value="nostr">nostr</option>
           <option value="other">Other</option>
         </select>
