bd07fca9d8babb2e023308f09613d0889ec8f372
Gigi <dergigi@pm.me>
2024-07-07 20:17:23 +0100

    Add "anything else?" to general application form

--- components/GrantApplicationForm.tsx
+++ components/GrantApplicationForm.tsx
@@ -295,6 +295,15 @@ export default function ApplicationForm() {
           {...register('references', { required: true })}
         />
       </label>
+      <hr />
+      <h2>Anything Else We Should Know?</h2>
+      <label className="block">
+        Feel free to share whatever else might be important.
+        <textarea
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('anything_else')}
+        />
+      </label>
       <div className="prose">
         <small>
           Open Sats may require each recipient to sign a Grant Agreement before
