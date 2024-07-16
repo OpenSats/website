efa2b27aa32ee3b48f0711f5c2eddedb75544a5e
Gigi <109058+dergigi@users.noreply.github.com>
2024-07-16 19:48:00 +0100

    Update prose of application form(s) (#290)
    * Add open-source byline to LTS form
    
    * Stronger legal language (remove "may")

--- components/GrantApplicationForm.tsx
+++ components/GrantApplicationForm.tsx
@@ -318,14 +318,14 @@ export default function ApplicationForm() {
       </label>
       <div className="prose">
         <small>
-          Open Sats may require each recipient to sign a Grant Agreement before
-          any funds are disbursed. Using the reports and presentations required
-          by the Grant Agreement, Open Sats will monitor and evaluate the
+          OpenSats requires each recipient to sign a Grant Agreement before any
+          funds are disbursed. Using the reports and presentations required by
+          the Grant Agreement, OpenSats will monitor and evaluate the
           expenditure of funds on a quarterly basis. Any apparent misuse of
           grant funds will be promptly investigated. If OpenSats discovers that
           the funds have been misused, the recipient will be required to return
-          the funds immediately, and be barred from further distributions. Open
-          Sats will maintain the records required by Revenue Ruling 56-304,
+          the funds immediately, and be barred from further distributions.
+          OpenSats will maintain the records required by Revenue Ruling 56-304,
           1956-2 C.B. 306 regarding distribution of charitable funds to
           individuals.
         </small>
