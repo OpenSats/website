9c190c10990094e1bcc61cdb2000bdd559a13403
Justin Ehrenhofer <12520755+SamsungGalaxyPlayer@users.noreply.github.com>
2024-11-18 16:48:35 -0600

    Increase max_amount (#89)

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 export const CURRENCY = 'usd'
 export const MIN_AMOUNT = 1
-export const MAX_AMOUNT = 5000
+export const MAX_AMOUNT = 100000
 export const AMOUNT_STEP = 5
 export const MEMBERSHIP_PRICE = 100
 export const POINTS_PER_USD = 1
