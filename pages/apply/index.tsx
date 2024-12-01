b1bd3e42e34dc09ba73af136f90f7755daa8eaf0
Gigi <109058+dergigi@users.noreply.github.com>
2024-12-01 15:15:15 +0000

    Revert "Revert #365"
    This reverts commit aea474bb15096985834015fd676563bc89ea0e31.

--- pages/apply/index.tsx
+++ pages/apply/index.tsx
@@ -6,6 +6,7 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import { MDXComponents } from '@/components/MDXComponents'
 import PageSection from '@/components/PageSection'
 import CustomLink from '@/components/Link'
+import ClosedNotice from '@/components/ClosedNotice'
 
 const DEFAULT_LAYOUT = 'PageLayout'
 
@@ -48,6 +49,7 @@ export default function Apply({
           information that is required to assess and judge the project or
           applicant.
         </p>
+        <ClosedNotice />
         <h2 className="mb-4 text-xl font-bold leading-normal md:text-2xl">
           General Grant
         </h2>
@@ -58,8 +60,8 @@ export default function Apply({
           projects in the Bitcoin space.
         </p>
         <Link
-          href="/apply/grant"
-          className="rounded border border-orange-500 bg-transparent px-4 py-2 font-semibold text-orange-500 no-underline hover:text-black dark:hover:text-white"
+          href="#/apply/grant"
+          className="disabled rounded border border-orange-500 bg-transparent px-4 py-2 font-semibold text-orange-500 no-underline hover:text-black dark:hover:text-white"
         >
           Apply for an OpenSats General Grant
         </Link>
@@ -72,8 +74,8 @@ export default function Apply({
           geared towards developers and maintainers of Bitcoin Core and similar.
         </p>
         <Link
-          href="/apply/lts"
-          className="rounded border border-orange-500 bg-transparent px-4 py-2 font-semibold text-orange-500 no-underline hover:text-black dark:hover:text-white"
+          href="#/apply/lts"
+          className="disabled rounded border border-orange-500 bg-transparent px-4 py-2 font-semibold text-orange-500 no-underline hover:text-black dark:hover:text-white"
         >
           Apply for an OpenSats LTS Grant
         </Link>
