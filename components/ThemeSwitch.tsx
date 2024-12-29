5913e49bb93a324347246cd0a17f9ee728b3c94c
chase-west <chasetwest@gmail.com>
2024-12-29 10:47:15 -0800

    Added smooth background transition

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -22,7 +22,7 @@ const ThemeSwitch = () => {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
-        className="text-gray-900 dark:text-gray-100 transition-colors duration-1000"
+        className="text-gray-900 dark:text-gray-100 transition-colors duration-300"
       >
         {mounted && (theme === 'dark' || resolvedTheme === 'dark') ? (
           <path
