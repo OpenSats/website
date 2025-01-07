082de50a19f9a9bec8d27e17babdc25bc2000c49
Gigi <dergigi@pm.me>
2025-01-07 07:41:13 -0600

    Introduce "Project Timeline" section

--- components/GrantApplicationForm.tsx
+++ components/GrantApplicationForm.tsx
@@ -117,6 +117,38 @@ export default function ApplicationForm() {
         />
       </label>
 
+      <hr />
+      <h2>Project Timeline</h2>
+
+      <label className="block">
+        Duration
+        <br />
+        <small>What's the grant duration you are applying for?</small>
+        <select
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('time_commitment', { required: true })}
+        >
+          <option value="100">12 months</option>
+          <option value="75">6 months</option>
+          <option value="50">3 months</option>
+          <option value="25">Other (please elaborate below)</option>
+        </select>
+      </label>
+
+      <label className="block">
+        Project Timeline and Potential Milestones *<br />
+        <small>
+          This will help us evaluate overall scope and potential grant duration.
+          (It's ok to pivot and/or work on something else, just let us know. For
+          now we want to see that you have a rough plan and you know what you're
+          doing.)
+        </small>
+        <textarea
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('timelines', { required: true })}
+        />
+      </label>
+
       <label className="block">
         Time Commitment
         <br />
@@ -130,20 +162,6 @@ export default function ApplicationForm() {
           <option value="50">50% - Part Time</option>
           <option value="25">25% - Side Project</option>
         </select>
-      </label>
-
-      <label className="block">
-        Project Timelines and Potential Milestones *<br />
-        <small>
-          This will help us evaluate overall scope and potential grant duration.
-          (It's ok to pivot and/or work on something else, just let us know. For
-          now we want to see that you have a rough plan and you know what you're
-          doing.)
-        </small>
-        <textarea
-          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
-          {...register('timelines', { required: true })}
-        />
       </label>
 
       <label className="block">
