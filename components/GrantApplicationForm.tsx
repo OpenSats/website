b7f7bc4bae45b4c1bcd016cadccc620beb4787c9
Gigi <109058+dergigi@users.noreply.github.com>
2025-01-08 08:07:30 -0600

    Add "Project Timeline" section to grant application form (#382)
    Co-authored-by: Jon Atack <jon@atack.com>
    Co-authored-by: Lucas <81401034+lucasmogu@users.noreply.github.com>

--- components/GrantApplicationForm.tsx
+++ components/GrantApplicationForm.tsx
@@ -118,32 +118,24 @@ export default function ApplicationForm() {
       </label>
 
       <label className="block">
-        Project Timelines and Potential Milestones *<br />
+        Project Website
+        <br />
         <small>
-          This will help us evaluate overall scope and potential grant duration.
-          (It's ok to pivot and/or work on something else, just let us know. For
-          now we want to see that you have a rough plan and you know what you're
-          doing.)
+          If you have a website or a project page, please provide the URL.
         </small>
-        <textarea
-          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
-          {...register('timelines', { required: true })}
-        />
-      </label>
-
-      <label className="block">
-        Project Website (if applicable)
         <input
           type="text"
+          placeholder="https://"
           className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
           {...register('website')}
         />
       </label>
 
       <hr />
+      <h2>Source Code</h2>
 
       <label className="block">
-        Project Github (or similar, if applicable)
+        Repository (GitHub or similar, if applicable)
         <input
           type="text"
           className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
@@ -177,6 +169,55 @@ export default function ApplicationForm() {
           .
         </small>
       </label>
+
+      <hr />
+      <h2>Project Timeline</h2>
+
+      <label className="block">
+        Duration
+        <br />
+        <small>Duration of grant you are applying for</small>
+        <select
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('duration', { required: true })}
+        >
+          <option value="12 months">12 months</option>
+          <option value="9 months">9 months</option>
+          <option value="6 months">6 months</option>
+          <option value="3 months">3 months</option>
+          <option value="Other">Other (please elaborate below)</option>
+        </select>
+      </label>
+
+      <label className="block">
+        Project Timeline and Potential Milestones *<br />
+        <small>
+          This will help us evaluate overall scope and potential grant duration.
+          (It's ok to pivot and/or work on something else, just let us know. For
+          now we want to see that you have a rough plan and you know what you're
+          doing.)
+        </small>
+        <textarea
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('timelines', { required: true })}
+        />
+      </label>
+
+      <label className="block">
+        Time Commitment
+        <br />
+        <small>How much time are you going to commit to the project?</small>
+        <select
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('commitment', { required: true })}
+        >
+          <option value="100%">100% - Full Time</option>
+          <option value="75%">75% - Part Time</option>
+          <option value="50%">50% - Part Time</option>
+          <option value="25%">25% - Side Project</option>
+        </select>
+      </label>
+
       <hr />
       <h2>Project Budget</h2>
 
