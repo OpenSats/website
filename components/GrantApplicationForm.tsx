11ced877ddafcf3bfb244b12cc66ac6dda68cfd6
Gigi <dergigi@pm.me>
2025-01-23 23:55:45 +0000

    Even stronger language

--- components/GrantApplicationForm.tsx
+++ components/GrantApplicationForm.tsx
@@ -160,9 +160,9 @@ export default function ApplicationForm() {
           {...register('license', { required: true })}
         />
         <small>
-          We focus on supporting projects that are free as in freedom and open
-          to all. Your project must have a proper open-source license &
-          educational materials must be available to the public under a{' '}
+          We only support projects that are free as in freedom and open to all.
+          Your project must have a proper open-source license & educational
+          materials must be available to the public under a{' '}
           <CustomLink href="https://www.gnu.org/licenses/license-list.html">
             free and open license
           </CustomLink>
