5af26c00d8437f516e5179f985fb387eeb453968
Jason Hong <jason.hong@protonmail.com>
2025-01-30 11:49:27 -0500

    refactored StatsBar as MDXComponent; fixed dark mode colors; edited fetch Google data error message

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -16,11 +16,13 @@ import DesignTeam from './DesignTeam'
 import Credits from './Supporters'
 import YouTubeEmbed from './YouTubeEmbed'
 import Members from './Members'
+import StatsBar from './StatsBar'
 
 export const Wrapper = ({ layout, content, ...rest }: MDXLayout) => {
   const Layout = require(`../layouts/${layout}`).default
   return <Layout content={content} {...rest} />
 }
+
 
 export const MDXComponents: ComponentMap = {
   Image,
@@ -38,4 +40,5 @@ export const MDXComponents: ComponentMap = {
   YouTubeEmbed,
   Credits,
   Members,
+  StatsBar,
 }
