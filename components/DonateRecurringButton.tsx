9ddad2e367a962115664954c26bf0683e38a7f76
Gigi <dergigi@pm.me>
2025-02-27 13:02:36 +0000

    Improve button appearance on hover

--- components/DonateRecurringButton.tsx
+++ components/DonateRecurringButton.tsx
@@ -2,12 +2,12 @@ import CustomLink from './Link'
 
 export default function DonateRecurringButton() {
   return (
-    <div className="mb-5 mt-10 flex justify-center">
+    <div className="mb-10 mt-12 flex justify-center">
       <CustomLink
         href="https://checkout.opensats.org/b/9AQbKVgQgb1ybba5kk"
-        className="my-15 rounded border border-orange-500 bg-transparent px-8 py-4 text-lg font-semibold text-orange-500 no-underline hover:border-transparent hover:bg-orange-500 hover:text-white"
+        className="my-15 rounded border border-orange-500 bg-transparent px-8 py-4 text-lg font-semibold no-underline hover:border-transparent hover:bg-orange-100"
       >
-        Donate Monthly 🧡
+        Give Monthly 🧡
       </CustomLink>
     </div>
   )
