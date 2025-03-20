0482f074d0d12648e199d94118a6397710956ffd
Jason Hong <jason.hong@protonmail.com>
2025-03-20 10:34:24 -0400

    add question for number of years dev experience on application forms

--- components/LTSApplicationForm.tsx
+++ components/LTSApplicationForm.tsx
@@ -108,6 +108,14 @@ export default function ApplicationForm() {
         />
       </label>
       <label className="block">
+        Number of Years of Developer Experience
+        <input
+          type="text"
+          className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
+          {...register('years_experience')}
+        />
+      </label>
+      <label className="block">
         Prior Contributions *<br />
         <small>
           Describe the contributions you've made to Bitcoin Core or other
