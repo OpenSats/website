353b64eb5d04e30e5414b3760eb84905d35761ef
Jason Hong <jason.hong@protonmail.com>
2025-03-24 14:02:03 -0400

    wording edit

--- components/LTSApplicationForm.tsx
+++ components/LTSApplicationForm.tsx
@@ -119,7 +119,7 @@ export default function ApplicationForm() {
         />
       </label>
       <label className="block">
-        Number of Years of Developer Experience
+        Years of Developer Experience
         <input
           type="text"
           className="mt-1 block w-full rounded-md border-gray-300 text-black shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
