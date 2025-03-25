6ebbd9b848d725acf54167de283fe21387e83f06
Gigi <109058+dergigi@users.noreply.github.com>
2025-03-25 21:06:19 +0000

    Re-open applications (#428)
    Revert "Close applications (#416)"
    
    This reverts commit c68e4c00d2cf9f9bd8490ca1b0e6072c5b2e4cd4.

--- components/LTSApplicationForm.tsx
+++ components/LTSApplicationForm.tsx
@@ -258,7 +258,7 @@ export default function ApplicationForm() {
       <FormButton
         variant={isFLOSS ? 'enabled' : 'disabled'}
         type="submit"
-        disabled={true || loading}
+        disabled={loading}
       >
         Submit LTS Application
       </FormButton>
