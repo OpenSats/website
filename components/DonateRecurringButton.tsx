9f19b642e447028093a660f015ff0175a852a134
Gigi <dergigi@pm.me>
2025-06-08 19:42:17 +0100

    Update link

--- components/DonateRecurringButton.tsx
+++ components/DonateRecurringButton.tsx
@@ -4,7 +4,7 @@ export default function DonateRecurringButton() {
   return (
     <div className="mb-10 mt-12 flex justify-center">
       <CustomLink
-        href="https://adtjkuks.donorsupport.co/page/opensats-general-fund"
+        href="https://adtjkuks.donorsupport.co/page/opensats"
         className="my-15 rounded border border-orange-500 bg-transparent px-8 py-4 text-lg font-semibold no-underline hover:bg-orange-100"
       >
         Give Monthly 🧡
