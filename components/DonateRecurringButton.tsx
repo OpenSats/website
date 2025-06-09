40b2098bd5750f9d3a14d3099cd5ad7e7750ede4
Gigi <109058+dergigi@users.noreply.github.com>
2025-06-09 19:03:29 +0100

    Basic FundraiseUp Integration (#454)
    * Use FundraiseUp link for recurring button
    
    * Update link

--- components/DonateRecurringButton.tsx
+++ components/DonateRecurringButton.tsx
@@ -4,7 +4,7 @@ export default function DonateRecurringButton() {
   return (
     <div className="mb-10 mt-12 flex justify-center">
       <CustomLink
-        href="https://checkout.opensats.org/b/4gwg1bbvW6Li0ww7st"
+        href="https://adtjkuks.donorsupport.co/page/opensats"
         className="my-15 rounded border border-orange-500 bg-transparent px-8 py-4 text-lg font-semibold no-underline hover:bg-orange-100"
       >
         Give Monthly 🧡
