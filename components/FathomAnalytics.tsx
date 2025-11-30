dd84c22daf62cd13c19a1416d0494aa188c3b7c2
Gigi <dergigi@pm.me>
2025-11-30 23:17:30 +0100

    Add Fathom Analytics tracking

---
+++ components/FathomAnalytics.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { load, trackPageview } from 'fathom-client'
+
+const FATHOM_SITE_ID = process.env.NEXT_PUBLIC_FATHOM_ID || 'UQPWIILQ'
+
+export default function FathomAnalytics() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!FATHOM_SITE_ID) return
+
+    load(FATHOM_SITE_ID, {
+      auto: false,
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!FATHOM_SITE_ID) return
+
+    const onRouteChangeComplete = () => {
+      trackPageview()
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router.events])
+
+  return null
+}
+
+
