97d5b654e7bc6c4a313a3c92cea08265e4a55d75
Gigi <109058+dergigi@users.noreply.github.com>
2025-11-30 23:24:27 +0100

    Add Fathom integration (#520)
    * Add Fathom Analytics tracking
    
    * Run Prettier formatting across project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import 'katex/dist/katex.css'
 // import '@docsearch/css' // Uncomment if using algolia docsearch
 import 'styles/globals.css'
 
-import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from 'pliny/analytics'
 import { SearchProvider } from 'pliny/search'
+import FathomAnalytics from '@/components/FathomAnalytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
+      <FathomAnalytics />
       <Analytics analyticsConfig={siteMetadata.analytics} />
       <LayoutWrapper>
         <SearchProvider searchConfig={siteMetadata.search}>
