70afa4517b8ad3da4d5817d79c28974497df4d88
Gigi <109058+dergigi@users.noreply.github.com>
2025-12-01 12:28:20 +0100

    Show related announcements for funds based on tags (#523)

--- pages/funds/[...slug].tsx
+++ pages/funds/[...slug].tsx
@@ -1,10 +1,13 @@
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import { MDXComponents } from '@/components/MDXComponents'
 import { InferGetStaticPropsType } from 'next'
-import { allFunds } from 'contentlayer/generated'
+import { allFunds, allBlogs } from 'contentlayer/generated'
+import type { Fund, Blog } from 'contentlayer/generated'
+import { sortedBlogPost, allCoreContent } from 'pliny/utils/contentlayer'
 import { useState } from 'react'
-import { Fund } from 'contentlayer/generated'
 import PaymentModal from '@/components/PaymentModal'
+import PostList from '@/components/PostList'
+import { getRelatedBlogPostsForFund } from '@/utils/relatedPosts'
 
 const DEFAULT_LAYOUT = 'ProjectLayout'
 
@@ -19,15 +22,20 @@ export const getStaticProps = async ({ params }) => {
   const slug = (params.slug as string[]).join('/')
   const project = allFunds.find((f) => f.slug === slug)
 
+  const sortedPosts = sortedBlogPost(allBlogs) as Blog[]
+  const relatedPosts = getRelatedBlogPostsForFund(project, sortedPosts)
+
   return {
     props: {
       project,
+      relatedPosts: allCoreContent(relatedPosts),
     },
   }
 }
 
 export default function FundPage({
   project,
+  relatedPosts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [modalOpen, setModalOpen] = useState(false)
   const [selectedFund, setSelectedFund] = useState<Fund>()
@@ -37,7 +45,6 @@ export default function FundPage({
   }
 
   function openPaymentModal() {
-    console.log('opening single fund modal...')
     setSelectedFund(project)
     setModalOpen(true)
   }
@@ -61,6 +68,17 @@ export default function FundPage({
         onRequestClose={closeModal}
         fund={selectedFund}
       />
+      {relatedPosts.length > 0 && (
+        <section className="mt-12 divide-y divide-gray-200 dark:divide-gray-700">
+          <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+            <div></div>
+            <h2 className="pb-8 text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 xl:col-span-2">
+              Related Announcements
+            </h2>
+          </div>
+          <PostList posts={relatedPosts} rightAlignDate useProjectLayout />
+        </section>
+      )}
     </>
   )
 }
