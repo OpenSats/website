442cc46f31730c01218032928b52f57031707fbf
Gigi <dergigi@pm.me>
2025-12-25 22:25:55 +0100

    style(year): remove search input placeholder text

--- pages/year/[year].tsx
+++ pages/year/[year].tsx
@@ -140,7 +140,7 @@ export default function YearPage({
                   aria-label="Search posts"
                   type="text"
                   onChange={(e) => setSearchValue(e.target.value)}
-                  placeholder="Search posts"
+                  placeholder=""
                   className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
                 />
               </label>
