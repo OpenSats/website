0c14bcc0e3025da8644ac70878c5b4f1cc13e53a
Gigi <dergigi@pm.me>
2025-12-25 16:14:46 +0100

    fix(map): remove heading

--- pages/map.tsx
+++ pages/map.tsx
@@ -85,9 +85,6 @@ export default function MapPage({
 
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-6 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            Grant map
-          </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             OpenSats has supported grantees in 40+ countries around the world.
           </p>
