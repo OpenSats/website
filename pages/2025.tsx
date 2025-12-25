faf4767d4ab0285b536bf10306a64c3a289790e5
Gigi <dergigi@pm.me>
2025-12-25 17:37:38 +0100

    feat: add /2025 page for blog posts from 2025

---
+++ pages/2025.tsx
@@ -0,0 +1,50 @@
+import siteMetadata from '@/data/siteMetadata'
+import ListLayout from '@/layouts/ListLayout'
+import { PageSEO } from '@/components/SEO'
+import { sortedBlogPost, allCoreContent } from 'pliny/utils/contentlayer'
+import { InferGetStaticPropsType } from 'next'
+import { allBlogs } from 'contentlayer/generated'
+import type { Blog } from 'contentlayer/generated'
+
+const YEAR = 2025
+const POSTS_PER_PAGE = 100
+
+export const getStaticProps = async () => {
+  const allPosts = sortedBlogPost(allBlogs) as Blog[]
+  const posts = allPosts.filter((post) => new Date(post.date).getFullYear() === YEAR)
+  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+
+  return {
+    props: {
+      initialDisplayPosts: allCoreContent(initialDisplayPosts),
+      posts: allCoreContent(posts),
+      pagination,
+    },
+  }
+}
+
+export default function Blog2025Page({
+  posts,
+  initialDisplayPosts,
+  pagination,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <PageSEO
+        title={`${YEAR} - ${siteMetadata.author}`}
+        description={`All blog posts from ${YEAR}`}
+      />
+      <ListLayout
+        posts={posts}
+        initialDisplayPosts={initialDisplayPosts}
+        pagination={pagination}
+        title={`${YEAR}`}
+      />
+    </>
+  )
+}
+
