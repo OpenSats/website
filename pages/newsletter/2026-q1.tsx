3767ab623d52b0ee708839251ea29d8de693f6f2
Gigi <dergigi@pm.me>
2025-12-29 10:56:56 +0100

    fix(newsletter): use heart icon instead of dollar on donate button

--- pages/newsletter/2026-q1.tsx
+++ pages/newsletter/2026-q1.tsx
@@ -441,7 +441,7 @@ export default function Newsletter2026Q1() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                   />
                 </svg>
                 Donate Monthly
