256816cb221c070e226547290398b60fb754f36a
Lucas <81401034+lucasmogu@users.noreply.github.com>
2026-03-30 19:35:50 +1030

    Stress the concept of opensource development
    I've noticed many grantees in the past that are not clear about the idea that every iteration of a truly and fully opensource project must be open and available at all times, not just the final version, so they tend to work privately on preliminary stages of the project with the intent to opensource only at the end. My intention here is to be very explicit about this issue.

--- components/GrantApplicationForm.tsx
+++ components/GrantApplicationForm.tsx
@@ -270,7 +270,9 @@ export default function ApplicationForm() {
           {...register('license', { required: true })}
         />
         <small>
-          We only support projects that are free as in freedom and open to all.
+          We only support projects that are free as in freedom and open to all,
+          from the very first draft all the way to the final code or educational material delivered,
+          everything produced under an OpenSats grant must be available to the public at all times.
           Your project must have a proper open-source license & educational
           materials must be available to the public under a{' '}
           <CustomLink href="https://www.gnu.org/licenses/license-list.html">
