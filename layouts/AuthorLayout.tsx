da5e1c251851ed7baca46b3c16e4a6560695d6e6
Gigi <dergigi@pm.me>
2026-04-01 18:50:51 +0100

    Remove volunteers section from /about page

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -66,11 +66,7 @@ export default function AuthorLayout({ children, content }: Props) {
               </span>
             </h3>
             <div className="text-gray-500 dark:text-gray-400">
-              {occupation == 'Volunteer' ? (
-                <CustomLink href="/about#volunteers">Volunteer</CustomLink>
-              ) : (
-                occupation
-              )}
+              {occupation}
             </div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
