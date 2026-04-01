c60553fefad6661d998f90c206df1d7f6c968fd3
Gigi <dergigi@pm.me>
2026-04-01 19:01:18 +0100

    style: format AuthorLayout with prettier

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -65,9 +65,7 @@ export default function AuthorLayout({ children, content }: Props) {
                 )}
               </span>
             </h3>
-            <div className="text-gray-500 dark:text-gray-400">
-              {occupation}
-            </div>
+            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               {email && <SocialIcon kind="mail" href={`mailto:${email}`} />}
