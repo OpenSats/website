b06c7383ea70bd85bf20f318c150853cf9a3cd15
Gigi <dergigi@pm.me>
2026-04-09 21:14:58 +0200

    style: revert social icon size back to 6 on project pages

--- layouts/ProjectLayout.tsx
+++ layouts/ProjectLayout.tsx
@@ -27,17 +27,17 @@ export default function PageLayout({ children, content }: Props) {
               className="h-48 w-48"
             />
             <div className="flex space-x-3 pt-6 [&>a]:opacity-50 [&>a]:transition-opacity [&>a]:hover:opacity-100">
-              <SocialIcon kind="website" href={website} size={7} />
+              <SocialIcon kind="website" href={website} size={6} />
               {twitter && (
                 <SocialIcon
                   kind="twitter"
                   href={`https://twitter.com/${twitter}`}
-                  size={7}
+                  size={6}
                 />
               )}
-              <SocialIcon kind="github" href={git} size={7} />
+              <SocialIcon kind="github" href={git} size={6} />
               {nostr && (
-                <SocialIcon kind="nostr" href={`https://njump.to/${nostr}`} size={7} />
+                <SocialIcon kind="nostr" href={`https://njump.to/${nostr}`} size={6} />
               )}
             </div>
           </div>
