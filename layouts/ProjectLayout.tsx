9009744601f08a749f158aafd43023ef768a4722
Gigi <dergigi@pm.me>
2026-04-09 21:35:22 +0200

    style: reorder social icons by relevance
    website → zapstore → github → nostr → twitter

--- layouts/ProjectLayout.tsx
+++ layouts/ProjectLayout.tsx
@@ -27,20 +27,20 @@ export default function PageLayout({ children, content }: Props) {
               className="h-48 w-48"
             />
             <div className="flex space-x-3 pt-6 [&>a]:opacity-50 [&>a]:transition-opacity [&>a]:hover:opacity-100">
+              <SocialIcon kind="website" href={website} size={6} />
               {zapstore && (
                 <SocialIcon kind="zapstore" href={zapstore} size={6} />
               )}
-              <SocialIcon kind="website" href={website} size={6} />
+              <SocialIcon kind="github" href={git} size={6} />
+              {nostr && (
+                <SocialIcon kind="nostr" href={`https://njump.to/${nostr}`} size={6} />
+              )}
               {twitter && (
                 <SocialIcon
                   kind="twitter"
                   href={`https://twitter.com/${twitter}`}
                   size={6}
                 />
-              )}
-              <SocialIcon kind="github" href={git} size={6} />
-              {nostr && (
-                <SocialIcon kind="nostr" href={`https://njump.to/${nostr}`} size={6} />
               )}
             </div>
           </div>
