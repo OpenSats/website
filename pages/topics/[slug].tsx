64fb5be6a6f9930cfcc93c2046abeab13076ce5a
Gigi <dergigi@pm.me>
2026-05-06 15:28:05 +0200

    fix(topics): point "Propose an edit" link to master branch
    The edit link on topic pages used `/edit/main/...`, but the repository's
    default branch is `master`, so the link 404'd. Switch to `/blob/master/...`
    to match the convention used by `PostLayout` and surface a working edit
    entry point on GitHub.

--- pages/topics/[slug].tsx
+++ pages/topics/[slug].tsx
@@ -54,7 +54,7 @@ export default function TopicPage({
   projectLink,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const repo = siteMetadata.siteRepo.replace(/\/$/, '')
-  const editUrl = `${repo}/edit/main/data/${topic.filePath}`
+  const editUrl = `${repo}/blob/master/data/${topic.filePath}`
   return (
     <>
       <MDXLayoutRenderer
