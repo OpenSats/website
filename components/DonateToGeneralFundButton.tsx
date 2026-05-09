f69bbeffcce40650cef2159b8b85ee6594316aee
dergigi <dergigi@pm.me>
2026-05-09 14:32:07 +0200

    refactor(donate): route legacy fund buttons through v2 banner

--- components/DonateToGeneralFundButton.tsx
+++ components/DonateToGeneralFundButton.tsx
@@ -1,39 +1,5 @@
-import { useState } from 'react'
-import { allFunds } from 'contentlayer/generated'
-import type { Fund } from 'contentlayer/generated'
-import PaymentModal from './PaymentModal'
+import DonateRecurringButton from './DonateRecurringButton'
 
 export default function DonateToGeneralFundButton() {
-  const [modalOpen, setModalOpen] = useState(false)
-  const [selectedFund, setSelectedFund] = useState<Fund>()
-  const generalFund = allFunds.find((p) => p.slug === 'general')
-
-  function closeModal() {
-    setModalOpen(false)
-  }
-
-  function openPaymentModal(fund: Fund) {
-    setSelectedFund(fund)
-    setModalOpen(true)
-  }
-
-  function openGeneralFundModal() {
-    openPaymentModal(generalFund)
-  }
-
-  return (
-    <>
-      <button
-        onClick={openGeneralFundModal}
-        className="mx-auto mb-2 mt-8 block rounded bg-orange-500 px-4 py-2 font-semibold text-white hover:border-transparent hover:bg-orange-500 hover:text-black dark:text-black dark:hover:text-white"
-      >
-        Donate to the General Fund
-      </button>
-      <PaymentModal
-        isOpen={modalOpen}
-        onRequestClose={closeModal}
-        fund={selectedFund}
-      />
-    </>
-  )
+  return <DonateRecurringButton />
 }
--- components/DonateToNostrFundButton.tsx
+++ components/DonateToNostrFundButton.tsx
@@ -1,39 +1,5 @@
-import { useState } from 'react'
-import { allFunds } from 'contentlayer/generated'
-import type { Fund } from 'contentlayer/generated'
-import PaymentModal from './PaymentModal'
+import DonateRecurringButton from './DonateRecurringButton'
 
 export default function DonateToNostrFundButton() {
-  const [modalOpen, setModalOpen] = useState(false)
-  const [selectedFund, setSelectedFund] = useState<Fund>()
-  const generalFund = allFunds.find((p) => p.slug === 'nostr')
-
-  function closeModal() {
-    setModalOpen(false)
-  }
-
-  function openPaymentModal(fund: Fund) {
-    setSelectedFund(fund)
-    setModalOpen(true)
-  }
-
-  function openNostrFundModal() {
-    openPaymentModal(generalFund)
-  }
-
-  return (
-    <>
-      <button
-        onClick={openNostrFundModal}
-        className="mx-auto mb-4 mt-8 block rounded bg-purple-500 px-4 py-2 font-semibold text-white hover:border-transparent hover:bg-purple-500 hover:text-black dark:text-black dark:hover:text-white"
-      >
-        Donate to The Nostr Fund
-      </button>
-      <PaymentModal
-        isOpen={modalOpen}
-        onRequestClose={closeModal}
-        fund={selectedFund}
-      />
-    </>
-  )
+  return <DonateRecurringButton designation="nostr" />
 }
