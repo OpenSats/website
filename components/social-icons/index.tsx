a2d59a659bd1041c33211ab5bab5bb3c7bf3da2c
dergigi <dergigi@pm.me>
2026-06-10 14:22:15 +0200

    feat(projects): theme nostr project pages
    Fixes OpenSats/content#253

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -43,7 +43,7 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-orange-500 dark:text-gray-200 dark:hover:text-orange-400 h-${size} w-${size}`}
+        className={`fill-current text-gray-700 hover:text-primary-600 dark:text-gray-200 dark:hover:text-primary-400 h-${size} w-${size}`}
       />
     </a>
   )
