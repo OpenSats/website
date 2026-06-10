fa97913b2aa6676dca34d4e0cfc3d5f127651dcc
Gigi <dergigi@pm.me>
2026-06-10 13:37:25 +0200

    style: rebalance vertical spacing around sats sent stat

--- layouts/ProjectLayout.tsx
+++ layouts/ProjectLayout.tsx
@@ -63,7 +63,7 @@ export default function PageLayout({
                 isFund ? 'hidden xl:block' : ''
               }`}
             />
-            <div className="flex justify-center gap-4 pt-5 xl:w-48 xl:justify-start [&_svg]:text-gray-400 [&_svg]:transition-colors dark:[&_svg]:text-gray-500">
+            <div className="flex justify-center gap-4 pt-6 xl:w-48 xl:justify-start [&_svg]:text-gray-400 [&_svg]:transition-colors dark:[&_svg]:text-gray-500">
               {zapstore && (
                 <SocialIcon kind="zapstore" href={zapstore} size={6} />
               )}
@@ -92,7 +92,7 @@ export default function PageLayout({
             </div>
             {totalSatsSent && (
               <div
-                className="pb-6 pt-20 text-center xl:w-48 xl:text-left"
+                className="pb-6 pt-14 text-center xl:w-48 xl:text-left"
                 title={`Total sats sent to ${title} by OpenSats`}
               >
                 <div className="inline-flex flex-col">
