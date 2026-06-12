67cb4749ca848dc700c969832f62f1e551a993fe
dergigi <dergigi@pm.me>
2026-06-12 10:12:22 +0200

    Revert "chore: TEMP force applications open for preview testing"
    This reverts commit 5b38f2c6f961d1c6a4d8f165a5a95135a219880a.

--- utils/applicationWindow.ts
+++ utils/applicationWindow.ts
@@ -1,6 +1,4 @@
 // Closed during the third month of each quarter (Mar, Jun, Sep, Dec).
 export function areApplicationsOpen(date = new Date()): boolean {
-  // TEMP: forced open for build preview testing. DROP THIS COMMIT BEFORE MERGE.
-  return true
   return date.getMonth() % 3 !== 2
 }
