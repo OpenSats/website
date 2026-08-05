db4814bc6edff093dd0197fbbace7013ea1057e8
Gigi <dergigi@pm.me>
2026-08-05 17:59:39 +0100

    feat(apply): prefix red GitHub issue titles with RED:

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -24,7 +24,7 @@ export default async function handler(
     console.log(`REPO: ${GH_ORG}/${GH_APP_REPO}`)
 
     const byOrFor = req.body.LTS ? 'for' : 'by'
-    const issueTitle = `${req.body.project_name} ${byOrFor} ${req.body.your_name}`
+    const issueTitle = `${req.body.RED ? 'RED: ' : ''}${req.body.project_name} ${byOrFor} ${req.body.your_name}`
 
     const contactFooter = `
 ---
