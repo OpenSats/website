7ad41192083c131ea815faeff01e265e6c8bab85
Gigi <dergigi@pm.me>
2026-08-20 14:12:40 +0200

    fix: require a positive amount to enable fiat donate

--- components/DonationForm.tsx
+++ components/DonationForm.tsx
@@ -52,7 +52,7 @@ const DonationSteps: React.FC<DonationStepsProps> = ({
   useEffect(() => {
     let fiatValid = false
     let btcValid: boolean
-    if (amount && typeof parseInt(amount) === 'number') {
+    if (Number(amount) > 0) {
       fiatValid = true
     }
     if (deductible === 'no' || (name && email)) {
